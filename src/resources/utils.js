b4b33498d6f6146805dd52296ee5636b8beff3cf
guyo68 <guy.ginzburg@gmail.com>
2023-07-15 12:10:11 +0300

    next date 20230722

--- src/resources/utils.js
+++ src/resources/utils.js
@@ -1,5 +1,5 @@
-const pastShifts = ["20230701AM", "20230701PM"];
-const lockedShifts = ["20230708AM", "20230708PM"];
+const pastShifts = ["20230701AM", "20230701PM", "20230708AM", "20230708PM"];
+const lockedShifts = ["20230715AM", "20230715PM"];
 const getPastShifts = (includeLocked) => includeLocked?pastShifts.concat(lockedShifts):pastShifts;
 const utils = {
     filterPastShifts: (shifts, includeLocked) => shifts.filter(shift => !getPastShifts(includeLocked).includes(shift.shiftId)),
